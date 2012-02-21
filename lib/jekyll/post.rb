887171f66b90c0218016fe8e1aa80da0a03ee8b3
Scott Weisman <sweisman18@gmail.com>
2012-02-20 19:49:37 -0600

    removed .html from posts via stammy

--- lib/jekyll/post.rb
+++ lib/jekyll/post.rb
@@ -91,7 +91,8 @@ module Jekyll
     #
     # Returns <String>
     def dir
-      File.dirname(url)
+      #File.dirname(url)
+      File.direname(generated_path)
     end
 
     # The full path and filename of the post.
@@ -120,25 +121,30 @@ module Jekyll
     # e.g. /2008/11/05/my-awesome-post.html
     #
     # Returns <String>
-    def url
-      return @url if @url
+    def generated_path
+      return permalink if permalink
 
-      url = if permalink
-        permalink
-      else
-        {
-          "year"       => date.strftime("%Y"),
-          "month"      => date.strftime("%m"),
-          "day"        => date.strftime("%d"),
-          "title"      => CGI.escape(slug),
-          "i_day"      => date.strftime("%d").to_i.to_s,
-          "i_month"    => date.strftime("%m").to_i.to_s,
-          "categories" => categories.map { |c| URI.escape(c) }.join('/'),
-          "output_ext" => self.output_ext
-        }.inject(template) { |result, token|
-          result.gsub(/:#{Regexp.escape token.first}/, token.last)
-        }.gsub(/\/\//, "/")
-      end
+      @generated_path ||= {
+        "year"       => date.strftime("%Y"),
+        "month"      => date.strftime("%m"),
+        "day"        => date.strftime("%d"),
+        "title"      => CGI.escape(slug),
+        "i_day"      => date.strftime("%d").to_i.to_s,
+        "i_month"    => date.strftime("%m").to_i.to_s,
+        "categories" => categories.join('/'),
+        "output_ext" => self.output_ext
+      }.inject(template) { |result, token|
+        result.gsub(/:#{Regexp.escape token.first}/, token.last)
+      }.gsub(/\/\//, "/")
+    end
+    
+    # The generated relative url of this post
+    # e.g. /2008/11/05/my-awesome-post
+    #
+    # Returns <String>
+    def url
+      site.config['multiviews'] ? generated_path.sub(/\.html$/, '') : generated_path
+    end
 
       # sanitize url
       @url = url.split('/').reject{ |part| part =~ /^\.+$/ }.join('/')
@@ -197,7 +203,7 @@ module Jekyll
     # Returns destination file path.
     def destination(dest)
       # The url needs to be unescaped in order to preserve the correct filename
-      path = File.join(dest, CGI.unescape(self.url))
+      path = File.join(dest, CGI.unescape(self.generated_path))
       path = File.join(path, "index.html") if template[/\.html$/].nil?
       path
     end
